513f2bfad11229ff5c92f30443d59a3c415b1616
alexanderhulteng <alexanderhulteng@gmail.com>
2021-08-26 17:03:29 -0600

    scraper can count items

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,6 +6,7 @@ def scraper
     url = "https://rateyourmusic.com/charts/"
     unparsed_page = open(url)
     parsed_page = Nokogiri::HTML(unparsed_page)
+    albums = parsed_page.css('div.topcharts_itembox.chart_item_release')
     byebug
 end
 
