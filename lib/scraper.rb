5508e9b9bfc079a4e5bbe5cc0f8de4c799a2f052
alexanderhulteng <alexanderhulteng@gmail.com>
2021-08-26 16:37:49 -0600

    start on scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,12 @@
 require 'nokogiri'
 require 'byebug'
-require 'httparty'
+require 'open-uri'
 
 def scraper
-    url = https://rateyourmusic.com/charts/
-    unparsed_page = HTTparty.get(url)
+    url = "https://rateyourmusic.com/charts/"
+    unparsed_page = open(url)
     parsed_page = Nokogiri::HTML(unparsed_page)
+    byebug
 end
 
 scraper
