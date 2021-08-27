81322f74e62304294f1d9ec6fdea2f556ba6126c
alexanderhulteng <alexanderhulteng@gmail.com>
2021-08-27 13:08:39 -0600

    fixing divs

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,16 +5,16 @@ require 'open-uri'
 
 
 def scraper
-    url = "https://rateyourmusic.com/charts/"
+    url = "https://www.allmusic.com/year-in-review/2020"
     unparsed_page = open(url)
     parsed_page = Nokogiri::HTML(unparsed_page)
-    albums = parsed_page.css('div.topcharts_itembox.chart_item_release')
+    albums = parsed_page.css('div.album')
     
-    albums.each do |a|
+    albums.each_with_index do |a, i|
         album = {
-            name: albums.css('div.topcharts_item_title').text.strip,
-            artist: albums.css('div.topcharts_item_artist_newmusicpage.topcharts_item_artist').text.strip,
-            position: albums.css('div.topcharts_position').text.strip
+            name: albums.css('h4.italic').text.strip,
+            artist: albums.css('h4').first.text.strip,
+            description: albums.css('p').text.strip
         }
         byebug
     end
@@ -23,9 +23,7 @@ end
 
 scraper
 
-#albumname: css('div.topcharts_item_title').text
-#artistname: css('div.topcharts_item_artist_newmusicpage.topcharts_item_artist')
-#releasedate: css('div.topcharts_item_releasedate')
-#genre: css('div.topcharts_item_genres_container')
-#position: css('div.topcharts_position')
-#rating: css('span.topcharts_stat.topcharts_avg_rating_stat')
+#albumname: css('h4.italic').text
+#artistname: css('h4').first.text
+#description: css('p').text
+
